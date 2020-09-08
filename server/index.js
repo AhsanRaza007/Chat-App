f55a43ea718660fa1500ff735622f1eff043b1fd
Raza <mdahsan.raza@dxc.com>
2020-09-08 16:40:35 +0530

    debugging server

--- server/index.js
+++ server/index.js
@@ -96,7 +96,8 @@ io.on("connection", socket => {
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
 
-  const root = require('path').join(__dirname,'../', 'client', 'build')
+  const root = require('path').join(__dirname, 'client', 'build')
+  console.log(root);
   app.use(express.static(root));
   app.get("*", (req, res) => {
       res.sendFile('index.html', { root });
