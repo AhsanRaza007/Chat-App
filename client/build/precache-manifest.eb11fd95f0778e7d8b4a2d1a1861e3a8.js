46a1ceb8b609fdebb05b6ef082f035088cc1c528
Raza <mdahsan.raza@dxc.com>
2020-09-08 17:47:22 +0530

    post deployed build removed

---
+++ client/build/precache-manifest.eb11fd95f0778e7d8b4a2d1a1861e3a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a1a8a512d6085f2480d910f369bd15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27a5e2d07023dd1363b7",
+    "url": "/static/css/2.3b460f5c.chunk.css"
+  },
+  {
+    "revision": "679f85c27ff6227e6280",
+    "url": "/static/css/main.ee94ce8a.chunk.css"
+  },
+  {
+    "revision": "27a5e2d07023dd1363b7",
+    "url": "/static/js/2.b20c2533.chunk.js"
+  },
+  {
+    "revision": "7df118ced395999f022fdbfb1eacbf1e",
+    "url": "/static/js/2.b20c2533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679f85c27ff6227e6280",
+    "url": "/static/js/main.0cc3ba9a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
